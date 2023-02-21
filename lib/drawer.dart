941e67f2078f6c71b76ff7aecb6780980c8ce451
Kishan <kishanrj@outlook.com>
2023-02-21 16:23:45 -0500

    Fixed some errors in drawer.dart

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DrawerContent extends StatefulWidget {
   @override
@@ -7,7 +8,25 @@ class DrawerContent extends StatefulWidget {
 }
 
 class _DrawerContentState extends State<DrawerContent> {
-  bool _showImage = true;
+  bool _showImage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSwitchState();
+  }
+
+  _loadSwitchState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _showImage = prefs.getBool('showImage') ?? true;
+    });
+  }
+
+  _saveSwitchState(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('showImage', value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,66 +34,74 @@ class _DrawerContentState extends State<DrawerContent> {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(26.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Planr Pro',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  Text(
-                    'Planr Pro',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  '- A Multi Use Task Planning App',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    '- A Multi Use Task Planning App',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Center(
-                    child: Container(
-                      padding: EdgeInsets.only(right: 15),
-                      child: Switch(
-                        value: _showImage,
-                        onChanged: (value) {
-                          setState(() {
-                            _showImage = value;
-                          });
-                        },
-                      ),),),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Center(
+                  child: Container(
                     padding: EdgeInsets.only(right: 15),
-                    child: _showImage
-                        ? Image.asset(
-                      'assets/images/planrpro_icon.png',
-                      height: 140,
-                      width: 140,
-                      fit: BoxFit.cover,
-                    )
-                        : Container(),
+                    child: Switch(
+                      value: _showImage,
+                      onChanged: (value) {
+                        setState(() {
+                          _showImage = value;
+                          _saveSwitchState(value);
+                        });
+                      },
+                    ),
                   ),
-                  SizedBox(
-                    height: 280,
-                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 200,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(right: 15),
+                  child: _showImage
+                      ? Image.asset(
+                    'assets/images/planrpro_icon.png',
+                    height: 140,
+                    width: 140,
+                    fit: BoxFit.cover,
+                  )
+                      : Container(),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
                   Container(
                     height: 1,
                     width: double.infinity,
@@ -93,8 +120,8 @@ class _DrawerContentState extends State<DrawerContent> {
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
