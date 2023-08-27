688cf6dc1da8106b72465079635e8c9f58b20726
kishanrajeev <kishanrj@outlook.com>
2023-08-27 16:58:16 -0400

    Adding notification functionality.

--- lib/page1.dart
+++ lib/page1.dart
@@ -4,6 +4,9 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:uuid/uuid.dart';
+import 'package:flutter/foundation.dart';
+
+
 
 class Page1 extends StatelessWidget {
   @override
